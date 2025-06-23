7e421be1a8de74558fbda6e91ce082778577d2a2
VICKY SHARMA <lucky170313@gmail.com>
2025-06-23 08:47:04 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <HomePage>
    
    with t

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+
 "use client";
 
 import Link from 'next/link';
@@ -53,7 +54,7 @@ export default function HomePage() {
   }, []);
 
   return (
-    <div className="flex flex-col pt-3 sm:pt-4">
+    <div className="flex flex-col">
       {/* Hero Section */}
       <section className="bg-background">
         <div className="container mx-auto px-4 sm:px-6 lg:px-8 py-8 sm:py-12">
