81b1311e563c5d762dd9d5e2ee478c5c4d460512
VICKY SHARMA <lucky170313@gmail.com>
2025-06-26 10:00:59 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/lib/mongodb.ts
+++ src/lib/mongodb.ts
@@ -3,17 +3,17 @@ import { MongoClient } from 'mongodb';
 // Add your MongoDB connection string to a .env.local file
 const MONGODB_URI = process.env.MONGODB_URI;
 
-if (!MONGODB_URI) {
-  throw new Error(
-    'Please define the MONGODB_URI environment variable inside .env.local'
-  );
-}
-
 let cachedClient: MongoClient | null = null;
 
 export async function connectToDatabase() {
   if (cachedClient) {
     return cachedClient;
+  }
+
+  if (!MONGODB_URI) {
+    throw new Error(
+      'Please define the MONGODB_URI environment variable inside .env.local'
+    );
   }
 
   const client = await MongoClient.connect(MONGODB_URI);
