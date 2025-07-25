f885e06d30bda93ca515ea0795249b7f49e75397
VICKY SHARMA <lucky170313@gmail.com>
2025-07-25 01:16:45 +0000

    use cach to save band width

--- next.config.ts
+++ next.config.ts
@@ -18,6 +18,19 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  async headers() {
+    return [
+      {
+        source: '/:all*(ico|png|svg|jpg|jpeg|gif|webp|woff|woff2)',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=31536000, immutable',
+          },
+        ],
+      },
+    ]
+  },
 };
 
 export default nextConfig;
